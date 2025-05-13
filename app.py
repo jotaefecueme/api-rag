6590e0e2f810d53d7e81a18f77e5dd8ceefc407f
jotaefecueme <50833571+jotaefecueme@users.noreply.github.com>
2025-05-13 18:23:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 app = FastAPI()
 
 embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_name="intfloat/multilingual-e5-large",
     model_kwargs={"device": "cpu"}, 
     encode_kwargs={"normalize_embeddings": True}
 )
